3bf28aaf80f9bce23448112bda08e0c6065bd52f
Serhii <sergiybiluk@gmail.com>
2020-08-11 20:45:34 +0200

    add fetch

--- src/App.js
+++ src/App.js
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+  handleClick() {
+    fetch('https://react-demo.azurewebsites.net/add').then(data => {
+      if (typeof data.json === 'function') {
+        data.json().then(elem => {
+          console.log({ elem })
+        })
+      } else {
+        console.log({ data })
+      }
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={this.handleClick}>
+            <img src={logo} className="App-logo" alt="logo" />
+          </button>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Keep calm and Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Keep calm and Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
