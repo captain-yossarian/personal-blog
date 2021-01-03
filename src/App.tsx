4203b33b659d4a82e4fc8bb7e1b8e588d30fc961
captain-yossarian <sergiybiluk@gmail.com>
2021-01-03 18:24:15 +0100

    add code example

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import React, { FC } from 'react';
+import React from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-twilight.css'
+import 'prismjs/components/prism-typescript';
 
+const code = `
+const foo: string = 'foo';
+const bar = 'bar';
+console.log(foo + bar);
+`.trim()
 
 function Home() {
   return <h2>Home</h2>;
 }
 
-function About() {
-  return <h2>About</h2>;
+
+class About extends React.Component {
+  componentDidMount() {
+    console.log({ prism: Prism.languages })
+    // You can call the Prism.js API here
+    // Use setTimeout to push onto callback queue so it runs after the DOM is updated
+    setTimeout(() => Prism.highlightAll(), 0)
+  }
+  render() {
+    return (
+      <pre className="line-numbers">
+        <code className="language-typescript">
+          {code}
+        </code>
+      </pre>
+    )
+  }
 }
 
-
-const App: FC = () => {
+const App = () => {
 
   return (
     <Router>
