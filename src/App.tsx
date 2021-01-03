dc22961dc49b9275358b1e03f60d9335cc139087
captain-yossarian <sergiybiluk@gmail.com>
2021-01-03 15:29:38 +0100

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React, { FC } from 'react';
-import './App.css';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
 
 
 const App: FC = () => {
 
   return (
-    <div>
-      blog
-
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
