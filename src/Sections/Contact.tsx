27839bcae1703c96f0d5c840c56043de7b80d209
captain-yossarian <sergiybiluk@gmail.com>
2021-01-11 09:37:37 +0100

    update Contact section

--- src/Sections/Contact.tsx
+++ src/Sections/Contact.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from "react";
+import { Anchor } from "../Shared/Links";
 
 const Contact: FC = () => (
   <>
     <div>
-      <p>sergiybiluk@gmail.com</p>
+      <Anchor href="mailto:sergiybiluk@gmail.com" text="My email" />
+      and
+      <Anchor
+        href="https://stackoverflow.com/users/8495254/captain-yossarian?tab=profile"
+        text="StackOverflow profile"
+      />
     </div>
   </>
 );
