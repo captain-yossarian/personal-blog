d88ff52441dd119fc6d31a01f0f65309570730be
captain-yossarian <sergiybiluk@gmail.com>
2021-01-12 15:18:59 +0100

    add twitter

--- src/Sections/Contact.tsx
+++ src/Sections/Contact.tsx
@@ -5,11 +5,12 @@ const Contact: FC = () => (
   <>
     <div>
       <Anchor href="mailto:sergiybiluk@gmail.com" text="My email" />
-      and
+      ,
       <Anchor
         href="https://stackoverflow.com/users/8495254/captain-yossarian?tab=profile"
         text="StackOverflow profile"
       />
+      and <Anchor href="https://twitter.com/sergiybiluk" text="twitter" />
     </div>
   </>
 );
