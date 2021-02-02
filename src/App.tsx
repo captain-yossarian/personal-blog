6b905bd47ed944f8ebae4b99fb1df7e26ea9480f
captain-yossarian <sergiybiluk@gmail.com>
2021-02-02 16:10:18 +0100

    change url

--- src/App.tsx
+++ src/App.tsx
@@ -67,9 +67,9 @@ const merged = Object.assign(blogArticles, sections);
 
 const map = Object.entries(merged);
 
-fetch(
-  "https://app-29b1f7f3-49c6-4695-8772-e0f34d438327.cleverapps.io/hey"
-).then((elem) => elem.json().then((data) => console.log({ data })));
+fetch("https://app-29b1f7f3-49c6-4695-8772-e0f34d438327.cleverapps.io/hey", {
+  mode: "no-cors",
+}).then((elem) => elem.json().then((data) => console.log({ data })));
 
 const App: VFC = () => (
   <Router>
