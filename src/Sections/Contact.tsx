3d1f3d1b3af9140e4cb73209b65c15b1aa70456e
captain-yossarian <sergiybiluk@gmail.com>
2021-04-28 07:54:50 +0200

    reduce contact

--- src/Sections/Contact.tsx
+++ src/Sections/Contact.tsx
@@ -5,17 +5,7 @@ const Contact: FC = () => (
   <>
     <div>
       <Anchor href="mailto:sergiybiluk@gmail.com" text="My email" />
-      ,
-      <Anchor
-        href="https://stackoverflow.com/users/8495254/captain-yossarian?tab=profile"
-        text="StackOverflow profile"
-      />
-      , <Anchor href="https://twitter.com/captainyosarian" text="twitter" />
-      and{" "}
-      <Anchor
-        href="https://www.linkedin.com/in/serhii-bilyk-764b6b7b/"
-        text="linkedin"
-      />
+      and <Anchor href="https://twitter.com/captainyosarian" text="twitter" />
     </div>
   </>
 );
