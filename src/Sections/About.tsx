80f4cb5914c26d14e1eedfe81acdb458f0171c4f
captain-yossarian <sergiybiluk@gmail.com>
2021-05-31 07:54:04 +0200

    update

--- src/Sections/About.tsx
+++ src/Sections/About.tsx
@@ -5,7 +5,7 @@ const About: FC = () => (
     <div>
       <p>Hi, my name is Serhii.</p>
       <p> I work as a software engineer at Intive.</p>
-      <p>Previously I had worked at UBS.</p>
+      <p>Previously I worked at UBS.</p>
       <p>
         If I used someone's code and did not provide link to original
         post/article/answer, please let me know, I will add it.
