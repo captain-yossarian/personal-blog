65aadda016a3d6edf616572cce78262f36e3c8a7
captain-yossarian <sergiybiluk@gmail.com>
2021-07-12 20:55:44 +0200

    add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -43,35 +43,35 @@ import { Main } from "./Layout";
 import ArticleBase from "./Shared/ArticleBase";
 
 export const componentMap = {
-  MathOperations,
-  ReactChildren,
-  ReactReturnType,
-  CompareArguments,
-  RangeNumbers,
-  Tuples,
-  UnionArray,
-  Callbacks,
-  PubSub,
-  TypeState,
-  Api,
-  About,
-  Contact,
-  Home,
-  Unions,
-  TemplateLiterals,
-  CallbackChain,
-  FlattenUnion,
-  Permutations,
-  Dates,
-  TypeNegation,
-  HexValidation,
-  LinkedList,
-  OOP,
-  FP,
-  DeepPick,
-  Validation,
-  Mutations,
-  ReactProps,
+  MathOperations: React.lazy(() => import("./Chapters/MathOperations")),
+  ReactChildren: React.lazy(() => import("./Chapters/ReactChildren")),
+  ReactReturnType: React.lazy(() => import("./Chapters/ReactReturnType")),
+  CompareArguments: React.lazy(() => import("./Chapters/CompareArguments")),
+  RangeNumbers: React.lazy(() => import("./Chapters/RangeNumbers")),
+  Tuples: React.lazy(() => import("./Chapters/Tuples")),
+  UnionArray: React.lazy(() => import("./Chapters/UnionArray")),
+  Callbacks: React.lazy(() => import("./Chapters/Callbacks")),
+  PubSub: React.lazy(() => import("./Chapters/PubSub")),
+  TypeState: React.lazy(() => import("./Chapters/TypeState")),
+  Api: React.lazy(() => import("./Chapters/Api")),
+  About: React.lazy(() => import("./Sections/About")),
+  Contact: React.lazy(() => import("./Sections/Contact")),
+  Home: React.lazy(() => import("./Sections/Home")),
+  Unions: React.lazy(() => import("./Chapters/Unions")),
+  TemplateLiterals: React.lazy(() => import("./Chapters/TemplateLiterals")),
+  CallbackChain: React.lazy(() => import("./Chapters/TemplateLiterals")),
+  FlattenUnion: React.lazy(() => import("./Chapters/FlattenUnion")),
+  Permutations: React.lazy(() => import("./Chapters/Permutations")),
+  Dates: React.lazy(() => import("./Chapters/Dates")),
+  TypeNegation: React.lazy(() => import("./Chapters/TypeNegation")),
+  HexValidation: React.lazy(() => import("./Chapters/HexValidation")),
+  LinkedList: React.lazy(() => import("./Chapters/LinkedList")),
+  OOP: React.lazy(() => import("./Chapters/OOP")),
+  FP: React.lazy(() => import("./Chapters/FunctionalProgramming")),
+  DeepPick: React.lazy(() => import("./Chapters/DeepPick")),
+  Validation: React.lazy(() => import("./Chapters/Validations")),
+  Mutations: React.lazy(() => import("./Chapters/Mutations")),
+  ReactProps: React.lazy(() => import("./Chapters/ReactProps")),
 };
 
 setTimeout(() => Prism.highlightAll(), 0);
@@ -98,25 +98,27 @@ const data = blogArticles
   .concat(sections);
 const App: VFC = () => {
   return (
-    <Router>
-      <Main>
-        <ScrollToTop />
-        <Switch>
-          {data.map((elem) => {
-            const { url, Comp } = elem;
-            const Component = componentMap[Comp as keyof typeof componentMap];
+    <React.Suspense fallback={<div>loading</div>}>
+      <Router>
+        <Main>
+          <ScrollToTop />
+          <Switch>
+            {data.map((elem) => {
+              const { url, Comp } = elem;
+              const Component = componentMap[Comp as keyof typeof componentMap];
 
-            return (
-              <Route path={url} key={url}>
-                <ArticleBase path={url} {...elem}>
-                  <Component />
-                </ArticleBase>
-              </Route>
-            );
-          })}
-        </Switch>
-      </Main>
-    </Router>
+              return (
+                <Route path={url} key={url}>
+                  <ArticleBase path={url} {...elem}>
+                    <Component />
+                  </ArticleBase>
+                </Route>
+              );
+            })}
+          </Switch>
+        </Main>
+      </Router>
+    </React.Suspense>
   );
 };
 
