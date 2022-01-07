0a5e505d8dc9c33d5fcc840f691438d6f68896de
captain-yossarian <sergiybiluk@gmail.com>
2022-01-07 20:54:25 +0100

    remove redundant text

--- src/Sections/About.tsx
+++ src/Sections/About.tsx
@@ -14,13 +14,10 @@ const About: FC = () => (
         If you noticed a mistake\typo, I kindly ask You to let me know. Thanks.
       </p>
       <p>
-        If you like my blog, you can <Anchor href="https://ko-fi.com/catchts" text="buy me a coffee" />
-      </p>
-      <p>
         <Anchor
           href="https://github.com/captain-yossarian/personal-blog"
           text="Here"
-        />{" "}
+        />
         you can find github repo of my blog.
       </p>
     </div>
