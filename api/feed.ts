f25c7dd48eee06947e8e6ee13888440230e3798e
captain-yossarian <sergiybiluk@gmail.com>
2022-01-08 21:47:21 +0100

    add rss feed

---
+++ api/feed.ts
@@ -0,0 +1,254 @@
+export const rss = `
+<rss version="2.0">
+<channel>
+<title>Catch TS</title>
+<link>https://catchts.com</link>
+<description>Here you can find some non-trivial typescript examples taken from real life.</description>
+<item>
+<title>Resolving generics in TypeScript</title>
+<link>https://catchts.com/generic-resolving</link>
+<guid>/generic-resolving</guid>
+<pubDate>2021-12-26T00:00:00.000Z</pubDate>
+<description>In this article you will learn TypeScript resolves generics</description>
+</item>
+<item>
+<title>Validation of static email string</title>
+<link>https://catchts.com/email-validation</link>
+<guid>/email-validation</guid>
+<pubDate>2021-12-13T00:00:00.000Z</pubDate>
+<description>In this article you will learn how to validate template literal type of email string</description>
+</item>
+<item>
+<title>Recursive data structure</title>
+<link>https://catchts.com/recursive-ds</link>
+<guid>/recursive-ds</guid>
+<pubDate>2021-11-17T00:00:00.000Z</pubDate>
+<description>How to put a limit on recursive data structure ?</description>
+</item>
+<item>
+<title>Rest operator in tuples (UPDATED)</title>
+<link>https://catchts.com/rest-tuples</link>
+<guid>/rest-tuples</guid>
+<pubDate>2021-10-20T00:00:00.000Z</pubDate>
+<description>Use rest operator one after another in tuple type representation</description>
+</item>
+<item>
+<title>Make enums safe again</title>
+<link>https://catchts.com/safe-enums</link>
+<guid>/safe-enums</guid>
+<pubDate>2021-10-06T00:00:00.000Z</pubDate>
+<description>You will learn how to make numerical enum safe again</description>
+</item>
+<item>
+<title>Typescript: Type Inference on function arguments (huge update)</title>
+<link>https://catchts.com/infer-arguments</link>
+<guid>/infer-arguments</guid>
+<pubDate>2021-09-18T00:00:00.000Z</pubDate>
+<description>Infer deep nested function arguments</description>
+</item>
+<item>
+<title>How to flatten a tuple type in TypeScript?</title>
+<link>https://catchts.com/flatten</link>
+<guid>/flatten</guid>
+<pubDate>2021-09-12T00:00:00.000Z</pubDate>
+<description>Flat literal deep nested tuple</description>
+</item>
+<item>
+<title>Define a recursive array of even length in TypeScript</title>
+<link>https://catchts.com/even-length</link>
+<guid>/even-length</guid>
+<pubDate>2021-09-04T00:00:00.000Z</pubDate>
+<description>Define a union of tuples with even length</description>
+</item>
+<item>
+<title>Safer TypeScript</title>
+<link>https://catchts.com/safer-types</link>
+<guid>/safer-types</guid>
+<pubDate>2021-09-02T00:00:00.000Z</pubDate>
+<description>Some tips and tricks about safer types</description>
+</item>
+<item>
+<title>Typing the inheritance chain</title>
+<link>https://catchts.com/prototype-typing</link>
+<guid>/prototype-typing</guid>
+<pubDate>2021-08-31T00:00:00.000Z</pubDate>
+<description>Recursive typing of inheritance</description>
+</item>
+<item>
+<title>Currying React components in TypeScript</title>
+<link>https://catchts.com/currying-components</link>
+<guid>/currying-components</guid>
+<pubDate>2021-07-20T00:00:00.000Z</pubDate>
+<description>React: functional approach</description>
+</item>
+<item>
+<title>Type negation in TypeScript</title>
+<link>https://catchts.com/type-negation</link>
+<guid>/type-negation</guid>
+<pubDate>2021-07-07T00:00:00.000Z</pubDate>
+<description>I want any type except ...</description>
+</item>
+<item>
+<title>Compute permutations in TypeScript</title>
+<link>https://catchts.com/permutations</link>
+<guid>/permutations</guid>
+<pubDate>2021-07-07T00:00:00.000Z</pubDate>
+<description>Compute permutations of enum keys and other unions</description>
+</item>
+<item>
+<title>Mutations in typescript</title>
+<link>https://catchts.com/mutations</link>
+<guid>/mutations</guid>
+<pubDate>2021-06-30T00:00:00.000Z</pubDate>
+<description>TypeScript is more about immutability</description>
+</item>
+<item>
+<title>OOP typings in TypeScript</title>
+<link>https://catchts.com/oop-style</link>
+<guid>/oop-style</guid>
+<pubDate>2021-06-24T00:00:00.000Z</pubDate>
+<description>Typings for classes</description>
+</item>
+<item>
+<title>Handle Props in React in TypeScript</title>
+<link>https://catchts.com/react-props</link>
+<guid>/react-props</guid>
+<pubDate>2021-06-19T00:00:00.000Z</pubDate>
+<description>Make illegal React props unrepresentable</description>
+</item>
+<item>
+<title>Deep pick typings (updated) in TypeScript</title>
+<link>https://catchts.com/deep-pick</link>
+<guid>/deep-pick</guid>
+<pubDate>2021-05-28T00:00:00.000Z</pubDate>
+<description>I will try to implement smth similar to lodash.get typings</description>
+</item>
+<item>
+<title>Functional programming. Typing compose and pipe functions in TypeScript</title>
+<link>https://catchts.com/FP-style</link>
+<guid>/FP-style</guid>
+<pubDate>2021-05-08T00:00:00.000Z</pubDate>
+<description>TypeScript typings for compose and pipe functions</description>
+</item>
+<item>
+<title>Callbacks</title>
+<link>https://catchts.com/callbacks</link>
+<guid>/callbacks</guid>
+<pubDate>2021-04-30T00:00:00.000Z</pubDate>
+<description>How to properly type callbacks in TypeScript</description>
+</item>
+<item>
+<title>Type validators in TypeScript</title>
+<link>https://catchts.com/validators</link>
+<guid>/validators</guid>
+<pubDate>2021-04-27T00:00:00.000Z</pubDate>
+<description>How to implements several type validators in generic way</description>
+</item>
+<item>
+<title>Linked list in TypeScript</title>
+<link>https://catchts.com/linked-list</link>
+<guid>/linked-list</guid>
+<pubDate>2021-04-26T00:00:00.000Z</pubDate>
+<description>Linked List with recursive types</description>
+</item>
+<item>
+<title>Handle literal arrays/tuples types in TypeScript</title>
+<link>https://catchts.com/tuples</link>
+<guid>/tuples</guid>
+<pubDate>2021-04-09T00:00:00.000Z</pubDate>
+<description>Variadic tuple types: filter, map, reduce, findIndex etc ...</description>
+</item>
+<item>
+<title>HEX Validation in TypeScript</title>
+<link>https://catchts.com/hex-validation</link>
+<guid>/hex-validation</guid>
+<pubDate>2021-03-29T00:00:00.000Z</pubDate>
+<description>Static type validator for HEX values</description>
+</item>
+<item>
+<title>Working with Dates in TypeScript</title>
+<link>https://catchts.com/dates</link>
+<guid>/dates</guid>
+<pubDate>2021-03-21T00:00:00.000Z</pubDate>
+<description>Type dates literal strings</description>
+</item>
+<item>
+<title>How to flatten union in TypeScript</title>
+<link>https://catchts.com/flatten-union</link>
+<guid>/flatten-union</guid>
+<pubDate>2021-02-11T00:00:00.000Z</pubDate>
+<description>Here you can find information about union flattening</description>
+</item>
+<item>
+<title>How to chain callbacks in TypeScript</title>
+<link>https://catchts.com/chain-callbacks</link>
+<guid>/chain-callbacks</guid>
+<pubDate>2021-02-10T00:00:00.000Z</pubDate>
+<description>Here you can find information about typing callbacks</description>
+</item>
+<item>
+<title>Publish subscribe pattern in TypeScript</title>
+<link>https://catchts.com/publish-subscribe</link>
+<guid>/publish-subscribe</guid>
+<pubDate>2021-01-25T00:00:00.000Z</pubDate>
+<description>Type safe publish subscribe pattern</description>
+</item>
+<item>
+<title>Working with template literals in TypeScript</title>
+<link>https://catchts.com/template-literals</link>
+<guid>/template-literals</guid>
+<pubDate>2021-01-22T00:00:00.000Z</pubDate>
+<description>Here you can find some advanced examples with template literals</description>
+</item>
+<item>
+<title>Api requests in TypeScript</title>
+<link>https://catchts.com/api</link>
+<guid>/api</guid>
+<pubDate>2021-01-11T00:00:00.000Z</pubDate>
+<description>Make type safe api requests with TypeScript</description>
+</item>
+<item>
+<title>Transform Union type to Tuple type in TypeScript</title>
+<link>https://catchts.com/union-array</link>
+<guid>/union-array</guid>
+<pubDate>2021-01-02T00:00:00.000Z</pubDate>
+<description>How to transform unions to array</description>
+</item>
+<item>
+<title>Type state pattern in TypeScript</title>
+<link>https://catchts.com/type-state</link>
+<guid>/type-state</guid>
+<pubDate>2020-12-23T00:00:00.000Z</pubDate>
+<description>Type safe handler</description>
+</item>
+<item>
+<title>Compare array arguments</title>
+<link>https://catchts.com/compare-arguments</link>
+<guid>/compare-arguments</guid>
+<pubDate>2020-12-18T00:00:00.000Z</pubDate>
+<description>Compare length of arrays as arguments in TypeScript</description>
+</item>
+<item>
+<title>Generate literal type for numbers range in TypeScript</title>
+<link>https://catchts.com/range-numbers</link>
+<guid>/range-numbers</guid>
+<pubDate>2020-12-15T00:00:00.000Z</pubDate>
+<description>How to generate numbers range as a literal type</description>
+</item>
+<item>
+<title>Math operations in TypeScript</title>
+<link>https://catchts.com/math</link>
+<guid>/math</guid>
+<pubDate>2020-12-13T00:00:00.000Z</pubDate>
+<description>How to make math operations either on number or bigint in TypeScript.</description>
+</item>
+<item>
+<title>Handle unions in React components in TypeScript</title>
+<link>https://catchts.com/unions</link>
+<guid>/unions</guid>
+<pubDate>2020-11-18T00:00:00.000Z</pubDate>
+<description>How to work with unions</description>
+</item>
+</channel>
+</rss>
+`
--- api/rss.ts
+++ api/rss.ts
@@ -1,6 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { rss } from './feed'
 
 export default (request: VercelRequest, response: VercelResponse) => {
     const { name } = request.query;
-    response.status(200).send(`Hello ${name}!`);
+    response.status(200).setHeader('Content-Type', 'text/xml')
+    response.send(rss)
 };
