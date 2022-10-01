808b7221d56daf5c3633ee27f7a354db87aaab67
Serhii Bilyk <serhii.bilyk@intellias.com>
2022-10-01 20:57:10 +0300

    fix ts downgrade issue

--- api/rss.ts
+++ api/rss.ts
@@ -1,7 +1,7 @@
-import type { VercelResponse } from '@vercel/node';
-import { rss } from './feed'
+import { VercelResponse } from "@vercel/node";
+import { rss } from "./feed";
 
 export default (_, response: VercelResponse) => {
-    response.status(200).setHeader('Content-Type', 'text/xml')
-    response.send(rss)
+  response.status(200).setHeader("Content-Type", "text/xml");
+  response.send(rss);
 };
