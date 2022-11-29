61c36eacd61e6bd67b6481ebcad23742874ec8bd
Serhii Bilyk <serhii.bilyk@intellias.com>
2022-11-29 11:13:44 +0200

    add handler

--- api/handler.ts
+++ api/handler.ts
@@ -2,6 +2,8 @@ import { VercelResponse, VercelRequest } from "@vercel/node";
 
 const handler = (request: VercelRequest, response: VercelResponse) => {
   console.log({ request });
+  response.status(200).setHeader("Content-Type", "text/xml");
+  response.send('<p>ok</p>');
 
 };
 
