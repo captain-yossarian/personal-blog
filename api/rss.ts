97d455f307ba0e83f27da4cbd320e8e3b2426e16
Serhii Bilyk <serhii.bilyk@intellias.com>
2023-05-15 11:09:26 +0300

    fix

--- api/rss.ts
+++ api/rss.ts
@@ -1,7 +1,6 @@
-import { type VercelResponse, type VercelRequest } from "@vercel/node";
 import { rss } from "./feed";
 
-export default (_: VercelRequest, response: VercelResponse) => {
+export default (_: any, response: any) => {
   response.status(200).setHeader("Content-Type", "text/xml");
   response.send(rss);
 };
