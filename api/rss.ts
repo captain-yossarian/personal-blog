90ee8089e3b06440f4ea596edf90a5e6b94b44db
Serhii Bilyk <serhii.bilyk@intellias.com>
2023-05-15 09:25:43 +0300

    fix nodejs

--- api/rss.ts
+++ api/rss.ts
@@ -1,8 +1,7 @@
 import { VercelResponse, VercelRequest } from "@vercel/node";
 import { rss } from "./feed";
 
-export default (request: VercelRequest, response: VercelResponse) => {
-  console.log({ request });
+export default (_: VercelRequest, response: VercelResponse) => {
   response.status(200).setHeader("Content-Type", "text/xml");
   response.send(rss);
 };
